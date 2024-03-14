65a13a237e16a2b7521c588eb3b8e4c2ed45be43
pileshwar <163389252+pileshwar@users.noreply.github.com>
2024-03-14 12:42:03 +0530

    Add files via upload

---
+++ module 3/condition.js
@@ -0,0 +1,45 @@
+// Write a program to take two numbers from the user and determine the greater of those two given numbers.
+//  const input = require("readline-sync");
+//  let a= input.questionInt("enter number = ");
+//  let b= input.questionInt("enter number = ");
+//  if(a>b){
+//      console.log("the  greater number is = ",a,);
+// } else
+//  console.log("the  small number is = ",b);
+
+//  Write a program to take a number from the user and print that number as Odd or Even.
+// const input = require("readline-sync");
+// let a= input.questionInt("enter number = ")
+// if(a%2){
+//   console.log("add",a);
+//  }
+//  else console.log("even",a);
+
+// Write a program to take a number from the user and output whether that number is negative, positive or zero.
+// const input = require("readline-sync");
+// let a= input.questionInt("enter number = ");
+// if(a>0){
+//     console.log("positive",a);
+// }else if(a<0){
+//     console.log("nagetive",a);
+// }
+// else console.log("zero",0);
+
+// Write a program to take an integer as input and print the smallest positive integer that is a multiple of both 2 and n.
+// const input = require("readline-sync");
+// let n = input.questionInt("enter number = ");
+// if (n%2){ 
+// a=n*2
+// console.log("number =",a)
+// }
+// else console.log("number =",n)
+
+// Write a program to take three numbers from the user and print the greater number of the three numbers.
+//  (Assume all three numbers are distinct)
+const input = require("readline-sync");
+let a = input.questionInt("enter number = ");
+let b = input.questionInt("enter number = ");
+let c = input.questionInt("enter number = ");
+if (a>b){
+    if (a>c)
+} 
---
+++ module 3/first.js
@@ -0,0 +1,86 @@
+// If the birth date is 22, the birth is May and the birth year is 2001 then
+// const input = require("readline-sync")
+// let birthdate = 22;
+// let  birthmonth = "may";
+// let birthyear = 2001;
+// console.log(birthdate);
+// console.log(birthmonth);
+// console.log(birthyear);
+
+// Write a program to take input from the user, and then print it to the output.
+// let user=12;
+// console.log(user);
+
+// Write a program to take two values from the user and swap them. 
+// const input = require("readline-sync")
+// let a = input.questionInt("enter number");
+// let b = input.questionInt("enter number");
+// a=a+b
+// b=a-b
+// a=a-b
+// console.log(a);
+// console.log(b);
+
+// Write a program to take two numbers from the user and perform Six basic 
+// operations (addition, subtraction, multiplication, division, integer division and modulus) on those two numbers.
+
+// Note: Print the output in the order as mentioned in the question.
+//  const input = require("readline-sync")
+//  let a = input.questionInt("enter number");
+//  let b = input.questionInt("enter number");
+//  c=a+b
+//  d=a-b
+//  e=a*b
+//  f=a/b
+//  j=a%b
+//  i=Math.floor(a/b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(j);
+// console.log(i);
+
+// Write a program to take two numbers A and B from the user and calculate 
+// the quotient and remainder when number A is divided by number B.
+
+// Note: Print the output in the order as mentioned in the question
+// const input = require("readline-sync")
+// let a = input.questionInt("enter number");
+// let b = input.questionInt("enter number");
+// p=Math.floor(a/b);
+// t=a%b
+// console.log(p);
+// console.log(t);
+
+// Write a program to take a positive number from the user and then display the last digit of that number.
+// const input = require("readline-sync")
+// let a = input.questionInt("enter number");
+// c=a%10
+// console.log(c);
+
+// Write a program to convert a temperature from Celsius to Fahrenheit using the formula C/5 = (F-32)/9
+// const input = require("readline-sync")
+// let c = input.question("enter number");
+//  f=c/5*9+32
+// console.log(f);
+
+// Write a program to take two inputs from the user and swap them without using a temporary or third variable.
+// const input = require("readline-sync")
+// let a = input.questionInt("enter number");
+// let b = input.questionInt("enter number");
+// c=a+b
+// a=c-a
+// b=c-b
+// console.log(a);
+// console.log(b);
+
+// Write a program to take two numbers, A and B from the user. Your task is to find the largest number
+//  that is less than A and can be divided evenly by B. Can you figure out that number?
+// const input = require("readline-sync");
+// let a= input.question("enter number");
+// let b= input.question("enter number");
+// c=Math.floor(a/b);
+// d=c*b
+// console.log(d);
+
